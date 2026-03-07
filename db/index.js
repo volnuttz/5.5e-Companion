f80ded60371c77e9fb05455a37b1ff02230ad61e
cucontre <cuauhtemoc.contreras@oracle.com>
2026-03-07 16:30:20 -0600

    database settings fix

--- db/index.js
+++ db/index.js
@@ -1,10 +1,8 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  database: 'dnd',
-  host: 'localhost',
-  port: 5432
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  : new Pool({ database: 'dnd', host: 'localhost', port: 5432 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
